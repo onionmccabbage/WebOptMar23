63f46a46bc097b7ebbe81f4284edd4897908cf16
onionmccabbage@live.com <onionmccabbage@live.com>
2023-03-03 13:11:01 +0000

    files to check stuff is working

---
+++ coderunner_check.js
@@ -0,0 +1,6 @@
+// in VS Code add the extesion called 'Code Runner'
+// Then run this code using the 'run' icon (usually top right corner of VSCode)
+console.log('if this prints to the console, then code runner works as intended')
+
+// next bit of code is for checking 'Live Server' works
+monitor.innerHTML = 'if this line appears in a browser then Live Server is working as intended'
