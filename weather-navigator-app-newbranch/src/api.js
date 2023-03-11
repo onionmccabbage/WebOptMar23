32c9583bba42d6b166f13fdee5a5e6db51808134
onionmccabbage@live.com <onionmccabbage@live.com>
2023-03-11 13:12:30 +0000

    weather app

---
+++ weather-navigator-app-newbranch/src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '50c757618bmshd3618990bec6443p12f2aajsnffd48d3c4ebe',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+      }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"; 
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "48f2d5e18b0d2bc50519b58cce6409f1";
