1790d214811a883d33ed5797049e577b5db77c47
onionmccabbage@live.com <onionmccabbage@live.com>
2023-03-13 12:30:51 +0000

    lunch day 1

---
+++ basics/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* use semantic tags for acccessibility */}
+      <article>
+        <header></header>
+        <nav></nav>
+        <section>
+          <aside>
+            <h3>Sample React App</h3>
+          </aside>
+        </section>
+        <footer></footer>
+      </article>
+    </div>
+  );
+}
+
+export default App;
