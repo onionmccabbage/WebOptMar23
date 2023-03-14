7d268d910616a5497cef466d39c4cf919cbdc1dc
onionmccabbage@live.com <onionmccabbage@live.com>
2023-03-14 10:34:29 +0000

    async-await and Fetch

--- ES_features/using_fetch.js
+++ ES_features/using_fetch.js
@@ -8,6 +8,9 @@ const fetchEmployees = async ()=>{ // async tells ES this is going to repond asy
     try {
         //               await is the part of async-await where  we specify the asynchronous code
         const response = await fetch(api) // fetch defaults to 'get'
+        // the response is everything from the server - we just need the JSON
+        const {data} = await response.json() // here we wait for a response, then strip the JSON into 'data'
+        return data
     } catch(error) {
         if (error){
             return error.message
@@ -17,4 +20,8 @@ const fetchEmployees = async ()=>{ // async tells ES this is going to repond asy
 
 // use our code
 fetchEmployees() // fetch is a Promise, so it is 'thenable'
-    .then( (d)=>{console.log(`we received ${d}`)} )
+    .then( (d)=>{
+        console.log(`we received ${d}`)
+        console.log(`member zero is ${d[0]['employee_name']}`)
+    } )
+// we can carry on with other stuff, the async-await will interrupt when it is ready
