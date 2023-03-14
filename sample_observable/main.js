aeff1cf9872ec970ad04afeab3495fedb64a69a6
onionmccabbage@live.com <onionmccabbage@live.com>
2023-03-14 13:53:09 +0000

    simple RxJS example

---
+++ sample_observable/main.js
@@ -0,0 +1,15 @@
+// we need to refer to bits of the DOM
+const search = document.getElementById('search') // ...as an HTMLInputElement
+
+// here are some useful keyboard codes (just printable characters)
+const isNum = (code)=>{return 32 && code <=126}
+
+// observables work best wth STREAMS of data, such as key-presses
+// we conventionaly use a trailing $ to identify an observable
+const keyup$ = Rx.Observable.fromEvent(document, 'keyup') // listen for keyboard events
+// the observable is only instantiated once there is at least one subscriber
+sub1 = keyup$.pluck('keyCode')
+    .filter(isNum) // only respond if the keyCode is 32-126
+    .subscribe( (keyCode)=>{
+        console.log(`user typed: ${String.fromCharCode(keyCode)}`)
+    } )
