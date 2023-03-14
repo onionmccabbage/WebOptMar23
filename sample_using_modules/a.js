866faf285b22a004fe64204c50af9fe5b3546792
onionmccabbage@live.com <onionmccabbage@live.com>
2023-03-14 11:54:47 +0000

    using import

--- sample_using_modules/a.js
+++ sample_using_modules/a.js
@@ -1,1 +1,7 @@
-import g from './b' 
+import DoStuff from './b' // the default export
+import {pi, phi} from './b' // non-defaults go in {}
+import DoStuff, {sqtTwo} from './b' // ...
+import * as util from './b' // avoid - not obvious
+
+// use the imported code
+console.log(`We have imported ${pi} ${phi} and ${sqtTwo}`)
--- sample_using_modules/b.js
+++ sample_using_modules/b.js
@@ -1,1 +1,11 @@
-export default g = 'HELLO'
+// variations on 'export'
+export var pi     = 3.14 // we tend to avoid var
+export let sqtTwo = 1.41
+export const phi  = 1.61
+export default class DoStuff{} // does nothing
+export const abso = (num)=>{
+    if (num<0){
+        num = num*-1 // always return postiive
+    }
+    return num
+}
